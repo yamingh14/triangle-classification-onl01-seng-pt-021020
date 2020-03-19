e912c24a0a066da385417cabe14c0590ab129282
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-19 05:03:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,40 +1,25 @@
-require 'pry'
 class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
-  def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
-  end
-
-  def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    else
+  def kind()
+    if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
       raise TriangleError
+    elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
+      raise TriangleError
+    elsif (@side1 == @side2) && (@side2 == @side3) :equilateral
+    elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3) :isosceles
+    elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3) calene
     end
   end
 end
 
 class TriangleError < StandardError
-
+  def message "TRIANGLE ERROR: GO STRAIGHT TO JAIL"
+  end
 end
