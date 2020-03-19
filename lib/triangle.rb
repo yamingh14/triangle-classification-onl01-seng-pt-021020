b9df3d5174471bd2c44b3ea311cd1e6da1347326
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-19 05:03:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,25 +1,28 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
-
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
   end
-
   def kind()
     if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
       raise TriangleError
     elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
       raise TriangleError
-    elsif (@side1 == @side2) && (@side2 == @side3) :equilateral
-    elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3) :isosceles
-    elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3) calene
+    else
+      if (@side1 == @side2) && (@side2 == @side3)
+        :equilateral
+      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+        :isosceles
+      elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3)
+        :scalene
+      end
     end
   end
-end
-
-class TriangleError < StandardError
-  def message "TRIANGLE ERROR: GO STRAIGHT TO JAIL"
+  class TriangleError < StandardError
+    def message
+      "TRIANGLE ERROR: GO STRAIGHT TO JAIL"
+    end
   end
 end
